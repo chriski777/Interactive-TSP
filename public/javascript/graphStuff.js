fb18ce90e377303ddc0652efb5bc4a674ed30e32
Chris <chriski777@berkeley.edu>
2018-08-06 02:11:05 -0700

    Implemented random initialization of graph instance.

--- public/javascript/graphStuff.js
+++ public/javascript/graphStuff.js
@@ -1,37 +1,65 @@
 $(document).ready(function() {
+
   var i,
-      s,
-      N = 20,
-      E = 100,
+      s;
+  initNewRandGraph(15);
+});
+
+function initNewRandGraph(nodeNum) {
+  var 
+      minX,
+      minY,
+      maxX,
+      maxY,
+      N = nodeNum,
+      // Graph must be a complete graph. Must have n*(n-1)/2 edges 
+      E = N*(N-1)/2,
       g = {
         nodes: [],
         edges: []
       };
+  minX = -50;
+  minY = -50;
+  maxX = 50;
+  maxY = 50;
   // Generate a random graph:
   for (i = 0; i < N; i++)
     g.nodes.push({
       id: 'n' + i,
       label: 'Node ' + i,
-      x: Math.random(),
-      y: Math.random(),
+      x: getRandInterval(minX, maxX),
+      y: getRandInterval(minX, maxX),
       size: getRandInterval(0.5,1),
       color: '#666'
     });
-  for (i = 0; i < E; i++)
-    g.edges.push({
-      id: 'e' + i,
-      source: 'n' + (Math.random() * N | 0),
-      target: 'n' + (Math.random() * N | 0),
-      size: getRandInterval(0.5,1),
-      color: '#ccc'
-    });
-  // Instantiate sigma:
+  console.log(g.nodes);
+  // Make sure it is a complete symmetric graph
+  // Include 0's for node to itself
+  var numEdges = 0;
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < N; j++) {
+      if (i != j && j > i) {
+        g.edges.push({
+          id: 'e' + numEdges++,
+          source: 'n' + i,
+          target: 'n' + j,
+          size: getRandInterval(0.5,1),
+          color: '#ccc'
+        });
+      };
+    }
+  }
+  //Clear and kill the past sigma instance
+  if (!(typeof s === "undefined")) {
+    s.kill()
+  }
+  // Instantiate sigma: 
   s = new sigma({
     graph: g,
     container: 'graph-container'
   });
+};
 
-  function getRandInterval(min, max) {
-    return Math.random() * (max - min) + min;
-  };
-});
+function getRandInterval(min, max) {
+  return Math.random() * (max - min) + min;
+};
--- public/javascript/webpage.js
+++ public/javascript/webpage.js
@@ -1,4 +1,30 @@
 $(document).ready(function() {
+
+	var minVal = 1;
+	var maxVal = 500;
+	// Get Started Dialog 
+	$( "#gs-dialog" ).dialog({
+		autoOpen: false,
+		width: 400,
+		buttons: [
+			{
+				text: "Okay",
+				click: function() {
+					$( this ).dialog( "close" );
+				}
+			},
+			{
+				text: "Cancel",
+				click: function() {
+					$( this ).dialog( "close" );
+				}
+			}
+		]
+	});
+	$( "#home" ).click(function( event ) {
+		$("#gs-dialog").dialog("open");
+		event.preventDefault();
+	});
 	//Random Graph Dialog
 	$( "#rand-dialog" ).dialog({
 		autoOpen: false,
@@ -7,7 +33,15 @@ $(document).ready(function() {
 			{
 				text: "Initialize",
 				click: function() {
-					$( this ).dialog( "close" );
+					var currVal = $("#node-input").val()
+					if (!(Math.floor(currVal) == currVal && $.isNumeric(currVal) ) || (currVal > maxVal || currVal < minVal) ) {
+						alert("Please input an integer between 1 and 500.");
+					} 
+					else {
+						console.log(currVal);
+						$( this ).dialog( "close" );
+						initNewRandGraph(currVal);
+					}
 				}
 			},
 			{
@@ -24,14 +58,14 @@ $(document).ready(function() {
 	});
 	//Node slider
 	$( "#node-slider" ).slider({
-		min: 1,
-		max: 500,
-		value: 10,
+		min: minVal,
+		max: maxVal,
+		value: $("#node-input").val(),
 		slide: function( event, ui ) {
-			$( "input" ).val( ui.value );
+			$( "#node-input" ).val( ui.value );
 		}
 	});
-	$("input").change(function () {
+	$("#node-input").change(function () {
 	    var nodeVal = this.value;
 	    console.log(nodeVal);
 	    $("#node-slider").slider("value", nodeVal);
